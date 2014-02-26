23a2aa49e389ea460ff1ac73547df2a23b2089c9
Hack Reactor <hackreactor@HR-A4.local>
2014-02-26 12:49:32 -0800

    functional queue completed, functional stack in progress

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,32 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var size =0;
   // Implement the methods below
 
   instance.enqueue = function(value){
+    for (var i = size ; i> 0; i--){
+      storage[size]=storage[size-1];
+    }
+    storage[0]=value;
+    size++;
   };
 
   instance.dequeue = function(){
+    if (size===0){
+      return null;
+    }
+    var result = storage[0];
+    for (var i=0; i< size-1; i++){
+      storage[i]=storage[i+1];
+    }
+    size--;
+    return result;
+
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size=0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size++]=value;
   };
 
   instance.pop = function(){
+    if (size===0){
+      return null;
+    }
+    var result = storage[size-1];
+    delete storage[size-1];
+    size--;
+    return result;
   };
 
   instance.size = function(){
+   return size;
   };
 
   return instance;
