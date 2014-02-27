7c7e5491df97b647d66d7f1c16e0f40deaecb361
Hack Reactor Students <justinyek@gmail.com>
2014-02-26 20:44:27 -0800

    Basic requirements done

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,26 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.len=0;
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this.storage[this.len++]=value;
+};
+
+stackMethods.pop = function(){
+  if (this.len===0){
+    return null;
+  }
+  var result = this.storage[this.len-1];
+  delete this.storage[this.len-1];
+  this.len--;
+  return result;
+};
+
+stackMethods.size = function(){
+  return this.len;
+};
