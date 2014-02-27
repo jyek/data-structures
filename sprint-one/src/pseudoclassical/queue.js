11d3b6427b64efda2265e103f0107a2914dd0d18
Hack Reactor Students <justinyek@gmail.com>
2014-02-26 20:29:17 -0800

    Pseudo queue done

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,27 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.instance = {};
+  this.storage = {};
+  this.len = 0;
 };
+
+Queue.prototype.enqueue = function(value){
+  for (var i = this.len; i > 0; i--){
+    this.storage[this.len]=this.storage[this.len-1];
+  }
+  this.storage[0] = value;
+  this.len++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.len === 0){
+    return null;
+  }
+  var result = this.storage[this.len - 1];
+  this.len--;
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this.len;
+};
+
